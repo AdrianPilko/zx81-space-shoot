08fc00bb125d69a883e8c7e3993e6532637095bc
Adrian Pilkington <50658445+AdrianPilko@users.noreply.github.com>
2024-10-12 19:16:59 +0100

    Update spaceShooter.asm

--- spaceShooter.asm
+++ spaceShooter.asm
@@ -18,7 +18,7 @@
 ; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ; SOFTWARE.
 
-;;; Maze game
+;;; Space shooter game
 ;;;
 ;;; https://youtube.com/@byteforever7829
 
